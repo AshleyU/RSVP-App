719fc6544f6a36114a7d044bf26c8ee5b60b6e54
Ashley U <ashley.umetsu@gmail.com>
2021-01-14 13:56:36 -0600

    Made thigns easier to read
    Split up some parts to make things easier to read and understand

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
-	const ul = document.getElementById('invitedList');
+const ul = document.getElementById('invitedList');
 
-
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const text = input.value;
-	input.value = '';
+function createLI(text) {
 	const li = document.createElement('li');
 	li.textContent = text;
 	const label = document.createElement('label');
@@ -16,6 +12,17 @@ form.addEventListener('submit', (e) => {
 	checkbox.type = 'checkbox';
 	label.appendChild(checkbox);
 	li.appendChild(label);
+	const button = document.createElement('button');
+	button.textContent = 'remove';
+	li.appendChild(button);
+	return li;
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const text = input.value;
+	input.value = '';
+	const li = createLI(text);
 	ul.appendChild(li);
 });
 
@@ -30,5 +37,13 @@ ul.addEventListener('change', (e) => {
 	} else {
 		listItem.className = '';
 
+	}
+});
+
+ul.addEventListener('click', (e) => {
+	if (e.target.tagName === 'BUTTON') {
+		const li = e.target.parentNode;
+		const ul = li.parentNode;
+		ul.removeChild(li);
 	}
 });
