a147cb6ebe4428c3ba66132b9e709670c1c963e5
Ashley U <ashley.umetsu@gmail.com>
2021-01-14 21:47:07 -0600

    More refactoring
    Simplified if statements and functions

--- app.js
+++ app.js
@@ -86,26 +86,34 @@ document.addEventListener('DOMContentLoaded', () => {
 			const button = e.target
 			const li = button.parentNode;
 			const ul = li.parentNode;
+			const action = button.textContent;
 
-			if (button.textContent === 'remove') {
-			ul.removeChild(li);
-			} else if (button.textContent === 'edit') {
-				const span = li.firstElementChild;
-				const input = document.createElement('input');
-				input.type = 'text';
-				input.value = span.textContent;
-				li.insertBefore(input, span);
-				li.removeChild(span);
-				button.textContent = 'save';
-			} else if (button.textContent === 'save') {
-				const input = li.firstElementChild;
-				const span = document.createElement('span');
-				span.textContent = input.value;
-				li.insertBefore(span, input);
-				li.removeChild(input);
-				button.textContent = 'edit';
+			const nameActions = {
+				remove: () => {
+				ul.removeChild(li);
+				},
+				edit: () => {
+					const span = li.firstElementChild;
+					const input = document.createElement('input');
+					input.type = 'text';
+					input.value = span.textContent;
+					li.insertBefore(input, span);
+					li.removeChild(span);
+					button.textContent = 'save';
+				},
+				save: () => {
+					const input = li.firstElementChild;
+					const span = document.createElement('span');
+					span.textContent = input.value;
+					li.insertBefore(span, input);
+					li.removeChild(input);
+					button.textContent = 'edit';
+					}
+				}
+
+				// select and run action in button's name
+				nameActions[action] ();
 			}
-		}
 	});
 });
 
